94eee043802bf322c67a0c7ebf24ab6931791893
ericymtai <ericymtai@Erics-MacBook-Air.local>
2016-12-07 08:51:07 -0500

    Read data from YouTube API and also create and write it to schedule playlist JSON file.

--- app.js
+++ app.js
@@ -12,29 +12,36 @@ var io = socketio.listen(server);
 router.use(express.static(path.resolve(__dirname, 'client')));
 
 // Variables to hold the messages and the sockets
-// var messages = [];
 var sockets = [];
-var fileList = "";
+var scheduleList = "";
+// var fileList = "";
+var scheduleResults = [];
 var playlist = [];
 
 io.on('connection', function (socket) {
     console.log('a user connected');
 
-    socket.on('request', function (songMeta) {
-      io.emit('request', songMeta);
-       console.log(songMeta);
-      // console.log("the song name is:" + songInfo[2]);
-      //console.log("the song name is: %j" + songInfo);
-
-
-
-      loadResults(songMeta);
+    socket.on('request', function (songMetaData) {
+        io.emit('request', songMetaData);
+        console.log(songMetaData);
+        loadResults(songMetaData);
     });
+
+
+    // socket.on('readSchedule', function ({scheduleResults}) {
+    //     socket.broadcast.emit('readSchedule', {scheduleResults});
+        // io.emit('readSchedule', scheduleResults);
+        // function checkProperty() {
+        //   console.log(scheduleResults[0]);
+        // }
+    //     console.log(scheduleResults[0]);
+    // });
 
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 });
+
 // Start our server
 server.listen(process.env.PORT || 8000, process.env.IP || "0.0.0.0", function () {
     var addr = server.address();
@@ -42,41 +49,66 @@ server.listen(process.env.PORT || 8000, process.env.IP || "0.0.0.0", function ()
 });
 
 
+fs.readdir('./schedule', function (err, files) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      scheduleList = files;
+      // console.log(scheduleList);
+      ParseJSON();
+});
+
+// Load the data from the playList.json
+function ParseJSON() {
+    // console.log(scheduleList[1]);
+    for (var i = 0, length = scheduleList.length; i < length; i++) {
+        //This loop will read through all of the elements of an array
+        var scheduleFile = fs.readFileSync('./schedule/' + scheduleList[i] + '', 'utf-8');
+        var json = checkJSON(scheduleFile);
+        if (json != undefined) {
+            scheduleResults.push(json);
+        }
+    }
+    checkProperty();
+}
+// console.log(scheduleResults[0]);
+
+// Try to find out which file is well JSON formated, if not pass throught and display error message
+function checkJSON(json) {
+  var data;
+  try {
+    data = JSON.parse(json)
+  } catch (e) {
+    // console.log(e);
+  }
+  return data;
+}
+
+//
+function checkProperty() {
+  // console.log(scheduleResults[0]);
+}
+
+
 
 // Load and combine all the files as a giant one
-function loadResults(songMeta) {
-    console.log("the song name is: " + songMeta.title);
-    console.log("the song image url is: " + songMeta.imgSrc);
-    console.log("the song ID is: " + songMeta.songId);
-
-    var fileList = JSON.stringify(songMeta);
-    console.log(fileList);
-
-      // fileList = songJson;
-
-
-        for (var i = 0, length = fileList.length; i < length; i++) {
-            //This loop will read through all of the elements of an array
-            var myFile = fs.readFileSync(fileList);
-            myFile = JSON.parse(myFile);
-            playList.push(myFile);
-        }
-        // saveFile();
-        // console.log("playList");
+function loadResults(songMetaData) {
+        playlist.push(songMetaData);
+        saveFile();
 }
 
 // Save the remain keys and their values into a new JSON file
 function saveFile() {
-      // console.log(voterResults);
-      var data = JSON.stringify(playList);
-
-      //Write data to a file
-      fs.writeFile('schedule/playList.json', data, function (err) {
-          if (err) {
-          console.log(err.message);
-          return;
-          }
-          // console.log('Saved the ' + voterResults[0].name + ' profile.');
-          console.log('Saved the new voterResults profile.');
-      });
+    // console.log(playList);
+    var data = JSON.stringify(playlist);
+    //console.log(data);
+    //Write data to a file
+    fs.writeFile('userSelect/score.json', data, function (err) {
+        if (err) {
+            console.log(err.message);
+            return;
+        }
+        console.log('Saved the new playList profile.');
+    });
 }
